c7f441dc0fd3fe894186816ee4b9d030400e98e6
Izabella Wojciechowska <wojciechowska.iza@tlen.pl>
2022-07-06 15:09:22 +0200

    lesson2 with task6 commited

--- lesson2/src/main/java/com/futurecollars/Main.java
+++ lesson2/src/main/java/com/futurecollars/Main.java
@@ -1,5 +1,9 @@
 package com.futurecollars;
 
 public class Main {
+    public static void main(String[] args) {
 
+        // to task6 - checking day of week
+        System.out.println("It's " + DayValidator.printDayOfWeek(2) + "!");
+    }
 }
