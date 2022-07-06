ec777eaee46b3013c2c25d35718b67600df120c6
Izabella Wojciechowska <wojciechowska.iza@tlen.pl>
2022-07-06 11:14:54 +0200

    lesson2 with task3 commited

---
+++ lesson2/src/main/java/com/futurecollars/Calculator.java
@@ -0,0 +1,21 @@
+package com.futurecollars;
+
+public class Calculator {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int substract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+}
--- lesson2/src/main/java/com/futurecollars/Main.java
+++ lesson2/src/main/java/com/futurecollars/Main.java
@@ -1,5 +1,9 @@
 package com.futurecollars;
 
 public class Main {
+    public static void main(String[] args) {
 
+        System.out.println(Calculator.sum(1,2));
+
+    }
 }
